dbe04fcc5cac81dba8de3f240c9c6a57b2cd3a46
Rajaguru Nataraj <86622240+Rajaguru03@users.noreply.github.com>
2024-08-23 12:58:04 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,4 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import '@testing-library,/json.com
+import '@testing-library,/json.com';
